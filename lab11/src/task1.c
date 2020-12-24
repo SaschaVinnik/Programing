e4000d3446c8b673d8b4909fb4020955120e0b30
SaschaVinnik <sascha.vinnik2015@gmail.com>
2020-12-24 19:36:18 +0200

    lab11and13maebidontWork

--- lab11/src/task1.c
+++ lab11/src/task1.c
@@ -1,21 +1,33 @@
 #include <malloc.h>
 #include <stdlib.h>
+#include <stdio.h>
 #define N 5
 #define M 3
 void fill_mas(int *mas1,int *mas2,int size1,int size2);
-void fill_mas3(int *mas1,int *mas2,int size1,int size2);
+void fill_mas3(int *mas1,int *mas2,int size1,int size2, int *mas3);
 void sort_mas3(int size1,int size2, int *mas3);
 int main ()
 {
-	int mas1[N] = {0};
-	int mas2[M] = {0};
-	int mas3[N+M] = {0};
+	//int mas1[N] = {0};
+	//int mas2[M] = {0};
+	//int mas3[N+M] = {0};
+	
+	int *mas1 = (int*)malloc(N * sizeof(int));
+	int *mas2 = (int*)malloc(M * sizeof(int));
+	int *mas3 = (int*)malloc(N+M * sizeof(int));
+	
 	fill_mas(mas1,mas2,N,M);
-	fill_mas3(mas1,mas2,N,M);
-	sort_mas3(N,M, int *mas3);
+	fill_mas3(mas1,mas2,N,M,mas3);
+	sort_mas3(N,M, mas3);
+	for (int i = 0; i < N + M; i++)
+	{
+		printf("%d", mas3[i]);
+	}
+	
 	return 0;
 }
 void fill_mas(int *mas1,int *mas2,int size1,int size2)
+{
 	for (int i = 0; i < size1; i++)
  	{
         	*(mas1 + i) = rand() % 10;
@@ -25,26 +37,27 @@ void fill_mas(int *mas1,int *mas2,int size1,int size2)
         	*(mas2 + i) = rand() % 10;
     	}
 }
-void fill_mas3(int *mas1,int *mas2,int size1,int size2)
+void fill_mas3(int *mas1,int *mas2,int size1,int size2, int *mas3)
 {
 	for (int i = 0; i < size1 ; i++)
 	{
 		*(mas3 + i) = *(mas1 + i);
 	}
-	for (int i = size1; i < size1 + saize2 ; i++)
+	for (int i = size1; i < size1 + size2 ; i++)
 	{
-		for(int j = 0; i < saize2 ; j++)
+		for(int j = 0; i < size2 ; j++)
 		{
 		*(mas3 + i ) = *(mas2 + j); 
 		}
+	}
 }
 void sort_mas3(int size1,int size2, int *mas3)
 {
 
     int temp = 0;
-    for (int i = 0; i < size1 + saize2; i++)
+    for (int i = 0; i < size1 + size2; i++)
     {
-        for(int j = 0; j < size1 + saize2; j++)
+        for(int j = 0; j < size1 + size2; j++)
         {
             if (*(mas3 + i) < *(mas3 + j)) 
             {
--- lab13/src/task1.c
+++ lab13/src/task1.c
@@ -3,31 +3,33 @@
 int main()
 {
 	char str [N] = "Then she usually reads a book or just watches TV.What a big building it is! What is this thing ? I am hungry?";
-	char sep1[10] = "!";
-	char sep2[10] = "?";
-	char sep3[10] = ".";
+	//char sep1[10] = "!";
+	//char sep2[10] = "?";
+	//char sep3[10] = ".";
+	
+	char sep[3] = {'!', '.', '?'};
 	char *istr;
 	int count1 = 0;
 	int count2 = 0;
 	int count3 = 0;
-	istr = strtok (str,sep1);
-	istr = strtok (str,sep2);
-	istr = strtok (str,sep3);
+	istr = strtok (str,sep[0]);
+	istr = strtok (str,sep[1]);
+	istr = strtok (str,sep[2]);
 	while (istr != NULL)
 	{
-		if (istr == sep1)
+		if (istr == sep[0])
 		{
 			count1++;
 		}
-		else if (istr == sep2)
+		else if (istr == sep[1])
 		{
 			count2++;
 		}
-		else if (istr == sep3)
+		else if (istr == sep[2])
 		{
 			count3++;
-		{
-		istr = (NULL,sep1,sep2,sep3);
+		}
+		istr = (NULL,sep[0],sep[1],sep[2]);
 	}
 	return 0;
 }
